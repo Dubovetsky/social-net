e880c634d626e8b634d78b54433090551357db19
Leo Dubovetsky <1st.ad.leone@gmail.com>
2020-07-07 11:57:15 +0300

    добавление store

--- src/Redux/state.js
+++ src/Redux/state.js
@@ -1,48 +1,56 @@
-  let rerenderEntireTree = () => {
-
-  }
-  let state = {
-      profilePage: {
-        posts: [
-            { id: 1, message: 'hi!', likesCount: 2 },
-            { id: 2, message: 'hello!', likesCount: 12 },
-            { id: 3, message: "WTF!", likesCount: 4 },
-          ],
-          newPostText: '',
-      },
-      dialogsPage: {
-        dialogs: [
-            { id: 1, name: 'John' },
-            { id: 2, name: 'Mary' },
-            { id: 3, name: 'Ivan' },
-            { id: 4, name: 'Chan' },
-            { id: 5, name: 'Victoria' },
-            { id: 6, name: 'Albert' }
-          ],
-          messages: [
-            { id: 1, message: 'Hi' },
-            { id: 2, message: 'Hello' },
-            { id: 3, message: "I'm ready" },
-            { id: 4, message: 'Hello' },
-            { id: 5, message: 'Hello' },
-          ],
-      }
-  }
-
-  window.state=state;
-
-  export const addPost = () => {
-      let newPost = {
-          id:5,
-          message: state.profilePage.newPostText,
-          likesCount:0,
-      };
-      state.profilePage.posts.push(newPost);
-      state.profilePage.newPostText='';
-      rerenderEntireTree(state);
-  }
-  export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText=newText;
-    rerenderEntireTree(state);
+let store = {
+  _state: {
+    profilePage: {
+      posts: [
+          { id: 1, message: 'hi!', likesCount: 2 },
+          { id: 2, message: 'hello!', likesCount: 12 },
+          { id: 3, message: "WTF!", likesCount: 4 },
+        ],
+        newPostText: '',
+    },
+    dialogsPage: {
+      dialogs: [
+          { id: 1, name: 'John' },
+          { id: 2, name: 'Mary' },
+          { id: 3, name: 'Ivan' },
+          { id: 4, name: 'Chan' },
+          { id: 5, name: 'Victoria' },
+          { id: 6, name: 'Albert' }
+        ],
+        messages: [
+          { id: 1, message: 'Hi' },
+          { id: 2, message: 'Hello' },
+          { id: 3, message: "I'm ready" },
+          { id: 4, message: 'Hello' },
+          { id: 5, message: 'Hello' },
+        ],
+    }
+  },
+  getState() {
+    return this._state
+  },
+  _callSubscriber() {
+    console.log('state changed');
+  },
+  addPost() {
+    let newPost = {
+        id:5,
+        message: this._state.profilePage.newPostText,
+        likesCount:0,
+    };
+    this._state.profilePage.posts.push(newPost);
+    this._state.profilePage.newPostText='';
+    this._callSubscriber(this._state);
+  },
+  updateNewPostText(newText) {
+    this._state.profilePage.newPostText=newText;
+    this._callSubscriber(this._state)
+  },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+  },
 }
-  export default state;
+
+window.store = store;
+
+export default store;
--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 import * as serviceWorker from './serviceWorker';
-import state from './Redux/state';
+import store from './Redux/state';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { addPost, updateNewPostText } from './Redux/state';
 
-let rerenderEntireTree = () => {
+let rerenderEntireTree = (state) => {
   ReactDOM.render(
     <React.StrictMode>
-      <App state={state} addPost={addPost} updateNewPostText={updateNewPostText} />
+      <App  state={store.getState} addPost={store.addPost.bind(store)} 
+            updateNewPostText={store.updateNewPostText.bind(store)} />
     </React.StrictMode>,
     document.getElementById('root')
   );
 }
 
-rerenderEntireTree(state); 
+rerenderEntireTree(store.getState); 
+
+store.subscribe (rerenderEntireTree);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
